4aae1e19839cf66429edcec7dba94ee0f8554657
Nahisah Nasleem <fathima.mohammad_nasleem@kcl.ac.uk>
2022-10-16 21:54:27 +0100

    Str) str git commit -m Str
    q
    :q

--- frequencies.py
+++ frequencies.py
@@ -5,8 +5,10 @@ def frequencies(items):
     frequencies = {}
     # Your code goes here
     for item in items:
-        if item not in frequencies.keys():
-            frequencies[item] = items.count(item)
+        if str(item) not in frequencies.keys():
+            frequencies[str(item)] = items.count(str(item)
 
 
     return frequencies
+
+print(frequencies(['0', 4,4,'4','d','d','e',0,'a','d','4']))
