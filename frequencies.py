b395fcdfc2112dd87de1ab5f466f15ba0cf5be43
Nahisah Nasleem <fathima.mohammad_nasleem@kcl.ac.uk>
2022-10-16 21:58:49 +0100

    Update dict

--- frequencies.py
+++ frequencies.py
@@ -3,12 +3,6 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
     for item in items:
-        if str(item) not in frequencies.keys():
-            frequencies[str(item)] = items.count(str(item)
-
-
+        frequencies[str(item)] = frequencies.get(str(item), 0) + 1
     return frequencies
-
-print(frequencies(['0', 4,4,'4','d','d','e',0,'a','d','4']))
