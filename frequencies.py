4a37ecaae0564b82cd20004053506c4a04b5276a
Nahisah Nasleem <fathima.mohammad_nasleem@kcl.ac.uk>
2022-10-16 21:39:44 +0100

    1st solution

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        if item not in frequencies.keys():
+            frequencies.update(item, items.count(item))
+
+
     return frequencies
